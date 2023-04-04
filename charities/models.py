0c2aeb50e2d20ba67e0bd86492bb441562845bd7
Mahdie Rahmati <rahmatimahdie9@gmail.com>
2023-04-05 02:13:08 +0430

    add task  model

--- charities/models.py
+++ charities/models.py
@@ -9,16 +9,24 @@ class Benefactor(models.Model):
     experience = models.SmallIntegerField(choices= EXPERIENCE_CHOICES , default= 0 )
     free_time_per_week = models.PositiveSmallIntegerField(default = 0 )
 
-
-    
-
-
 class Charity(models.Model):
-    pass
-
-
-class Task(models.Model):
     user = models.OneToOneField(User)
     name = models.CharField(max_length=50)
     reg_number = models.CharField(max_length=10)
+
+
+class Task(models.Model):
+    GENDER_CHOICES = [ "M" , "F"]
+    STATE_CHOICES = ["P" , "W" , "A" , "D"]
+    assigned_benifactor  = models.ForeignKey(Benefactor , null=True , on_delete= models.SET_NULL)
+    charity = models.ForeignKey(Charity)
+    age_limit_from = models.IntegerField(blank = True)
+    age_limit_to = models.IntegerField(blank = True)
+    date = models.DateField(blank = True)
+    description = models.TextField(blank = True)
+    gender_limit = models.CharField(max_length = 1 , choices= GENDER_CHOICES , blank= True)
+    state = models.CharField(default = "P" , choices=STATE_CHOICES)
+    title = models.CharField(max_length = 60) 
+
+
     
