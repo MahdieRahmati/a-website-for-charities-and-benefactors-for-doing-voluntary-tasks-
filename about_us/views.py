6948c69e2809d498ddd4a1eb1fcf0b5669b4260d
Mahdie Rahmati <rahmatimahdie9@gmail.com>
2023-05-22 18:01:29 +0430

    added a new func for about-us

--- about_us/views.py
+++ about_us/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from accounts.models import User
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django.shortcuts import render
+
 
 def about_us(request):
-    # View for about-us page
-    user_queryset = User.objects.all()
-    full_name_list = []
-    for user in user_queryset:
-        full_name_list.append(user.get_full_name())
+    context = {
+        'members': get_user_model().objects.all()
+    }
+    return render(request, 'about_us.html', context)
 
-    return render(request , "about_us.html" , context = {"full_name_list" : full_name_list})
+# Create your views here.
+
+# def about_us(request):
+#     # View for about-us page
+#     user_queryset = User.objects.all()
+#     full_name_list = []
+#     for user in user_queryset:
+#         full_name_list.append(user.get_full_name())
+
+#     return render(request , "about_us.html" , context = {"full_name_list" : full_name_list})
 
 
