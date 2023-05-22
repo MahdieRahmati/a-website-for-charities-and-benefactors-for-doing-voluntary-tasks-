b9f4fe1bbd4acb4b4f4395d0ddb7c32d5a0bdd4c
Mahdie Rahmati <rahmatimahdie9@gmail.com>
2023-05-22 19:44:34 +0430

    added admin panel for charities

--- charities/admin.py
+++ charities/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from charities.models import Charity , Benefactor , Task
 # Register your models here.
+
+admin.site.register(Charity)
+admin.site.register(Benefactor)
+admin.site.register(Task)
